8c95fc5fe18bee66a2764234c637d0a00ba1fa4a
Cyrielle Thomas <cyriellethomas@Cyrielle.local>
2023-07-20 12:09:07 +0200

    Mise en place de la selection d'une famille et des boutons ajouter, modifier et supprimer

--- frontend/src/pages/Admin/AdminFamilles.jsx
+++ frontend/src/pages/Admin/AdminFamilles.jsx
@@ -1,21 +1,56 @@
 import React, { useState, useEffect } from "react";
+import connexion from "../../services/connexion";
 
 function AdminFamilles() {
+  const familleModel = { id: null, name: "" };
+  const [familles, setFamilles] = useState([]);
+  const [famille, setFamille] = useState({ familleModel });
+
+  const handleFamille = (id) => {
+    if (id === "") {
+      setFamille(familleModel);
+    } else {
+      setFamille(familles.find((fam) => fam.id === +id));
+    }
+  };
+  const getFamilles = async () => {
+    try {
+      const famillesList = await connexion.get(`/familles`);
+      setFamilles(famillesList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getFamilles();
+  }, []);
+
   return (
     <div className="w-full flex flex-col">
       <div className=" w-full text-center font-amatic text-3xl my-10 underline">
         Administration des familles
       </div>
       <div className="flex flex-col py-20 ml-10">
-        <select className="w-1/4 text-center mb-10">
-          Selectionnez la famille
-          <option>Option1</option>
+        <select
+          value={famille.id}
+          className="w-96 text-center mb-10 font-amatic text-2xl "
+          onChange={(e) => handleFamille(e.target.value)}
+        >
+          <option className="mb-4" value="">
+            Selectionnez la famille à modifier
+          </option>
+          {familles.map((fam) => (
+            <option key={fam.id} value={fam.id}>
+              {fam.name}
+            </option>
+          ))}
         </select>
         <label htmlFor="famille" className="ml-2 font-amatic text-2xl">
           Famille
         </label>
         <input
-          value=""
+          value={famille.name}
           type="text"
           name="Famille"
           id="famille"
@@ -23,12 +58,30 @@ function AdminFamilles() {
           placeholder="Nom de la famille (ex: Epicerie, droguerie)"
           required
         />
-        <button
-          type="button"
-          className="border-2 bg-green border-green rounded-xl w-28 h-10 text-white mt-6"
-        >
-          AJOUTER
-        </button>
+        {!famille.id && (
+          <button
+            type="button"
+            className="border-2 bg-green border-green rounded-xl w-28 h-10 text-white mt-6"
+          >
+            AJOUTER
+          </button>
+        )}
+        {famille.id && (
+          <div className="flex w-96 flex-row justify-around">
+            <button
+              type="button"
+              className="border-2 bg-green border-green rounded-xl w-28 h-10 text-white mt-6"
+            >
+              MODIFIER
+            </button>
+            <button
+              type="button"
+              className="border-2 bg-green border-green rounded-xl w-28 h-10 text-white mt-6"
+            >
+              SUPPRIMER
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
